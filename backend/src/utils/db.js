1e1f5c95ea7a10f8878ce1b50468f0929aed705a
ouattara <ouattaraibrahimyacouba@gmail.com>
2025-08-04 01:57:19 +0000

    modification vercel backend

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 // Connect to MongoDB
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect(process.env.DATABASE_URL, {
+            socketTimeoutMS: 30000, connectTimeoutMS: 30000
+        });
         console.log("Connexion à la base de donnée reussie!");
     } catch (error) {
         console.error(`Erreur de connexion à la base de donnée: ${error.message}`);
