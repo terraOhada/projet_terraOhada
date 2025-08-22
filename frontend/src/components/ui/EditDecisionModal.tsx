eb562876bab4910a75cc1457a8dd1881e7669603
ouattara <ouattaraibrahimyacouba@gmail.com>
2025-08-22 09:02:05 +0000

    modification typeDecision 2

--- frontend/src/components/ui/EditDecisionModal.tsx
+++ frontend/src/components/ui/EditDecisionModal.tsx
@@ -391,10 +391,10 @@ const EditDecisionModal: React.FC<EditDecisionModalProps> = ({
               variants={formItemVariants}
             >
               <label className="block text-sm font-medium text-gray-700">
-                Colonne 1
+                typeDecision
               </label>
               <input
-                name="colonne1"
+                name="typeDecsion"
                 type="text"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm transition-all"
                 value={""}
