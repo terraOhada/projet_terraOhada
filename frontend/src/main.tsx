7e24260228dd04948609c2aeda295bf0c6ec9d69
ouattara <ouattaraibrahimyacouba@gmail.com>
2025-11-22 16:13:48 +0100

    ajout de google analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,20 @@ import App from "./App.tsx";
 import { Toaster } from "react-hot-toast";
 import { SkeletonTheme } from "react-loading-skeleton";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AnalyticsProvider } from "@keiko-app/react-google-analytics";
 
 const queryClient = new QueryClient();
+const analyticsConfig = {
+  mesureId: import.meta.env.VITE_APP_GA4_MEASUREMENT_ID,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <SkeletonTheme>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <AnalyticsProvider config={analyticsConfig}>
+          <App />
+        </AnalyticsProvider>
       </QueryClientProvider>
     </SkeletonTheme>
     <Toaster position="top-center" reverseOrder={false} />
